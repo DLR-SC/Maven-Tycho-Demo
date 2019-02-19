29f1a8439c86feb9d3d54b2a220705418ed225e9
Philipp M. Fischer <35496033+PhilMFischer@users.noreply.github.com>
2019-02-19 23:55:47 +0100

    Feature/task 6 swt bot ui tests on tra (#18)
    - Added plugin for UI tests
    - Updated maven tycho build process
    - Made tests executable in IDE and in maven build
    - Implemented example test case
    - Added launchers for IDE
    - Updated target platform
    - Enabled XVFB service in travis.yml
    - Added metacity window manager to tarvis.yml - (Task #6)
    - Missed setting release notes for 0.2.0

---
+++ de.dlr.maven.tycho.demo.swtbot.test/src/de/dlr/maven/tycho/demo/swtbot/test/ProjectUiAllTest.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2008-2018 German Aerospace Center (DLR), Simulation and Software Technology, Germany.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package de.dlr.maven.tycho.demo.swtbot.test;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * 
+ * Collection of SWT Bot Test Cases
+ *
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	RCPMavenTychoDemoUiTests.class
+				})
+
+public class ProjectUiAllTest {
+
+}
